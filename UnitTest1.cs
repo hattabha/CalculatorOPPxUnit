43559bc26141d3673b5442674d7bf9f503fbb0ab
hattabha <84962132+hattabha@users.noreply.github.com>
2021-07-06 10:25:37 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CalculatorXunitTest
+{
+    public class UnitTest1
+    {
+        
+       public void Test1()
+        {
+            
+        } 
+       
+    }
+}
